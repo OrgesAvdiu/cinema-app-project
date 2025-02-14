f0dcf910f1374cea74f06898ef8a4a60d3247f50
Drenniii <149413647+Drenniii@users.noreply.github.com>
2025-02-14 18:21:08 +0100

    City view

---
+++ web-app/src/pages/admins/CityView.js
@@ -0,0 +1,44 @@
+import CustomMaterialTable from "../../component/dashboard/CustomMaterialTable";
+import { useRef } from "react";
+import { SelectTableCell, TextFieldTableCell } from "../../component/TableCells";
+import { QueryKeys } from "../../services/QueryKeys";
+import { CityService } from "../../services/CityService";
+import { OffersService } from "../../services/OffersService";
+import { useQuery } from "react-query";
+
+const cityService = new CityService();
+const offerService = new OffersService();
+export default function CityView({}) {
+  const errorRef = useRef();
+const {data: allOfers} = useQuery(QueryKeys.OFFER, () => offerService.findAll());
+  const columns = [
+   {
+      title: "Name",
+      field: "name",
+      editComponent: (props) => TextFieldTableCell(props, errorRef),
+   },
+   {
+    title: "Offer",
+    field: "offer",
+    render: (rowData) => rowData.offer?.Title,
+    editComponent: (props) =>
+      SelectTableCell(
+        props,
+        errorRef,
+        allOfers?.map((x) => ({ value: x, label: x.name })) || [],
+        "id",
+      ),
+  },
+  ];
+
+  return (
+    <CustomMaterialTable
+      title="Manage Cities"
+      columns={columns}
+      service={cityService}
+      queryKey={QueryKeys.CITY}
+      errorRef={errorRef}
+      // disableDeleteAction
+    />
+  );
+}
