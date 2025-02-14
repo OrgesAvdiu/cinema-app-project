ad9f4f7b78c4b213c16f7a27179960ed986ae291
Drenniii <149413647+Drenniii@users.noreply.github.com>
2025-02-14 14:40:13 +0100

    Offers view

---
+++ web-app/src/pages/admins/OffersView.js
@@ -0,0 +1,79 @@
+import CustomMaterialTable from "../../component/dashboard/CustomMaterialTable";
+import { useRef } from "react";
+import {useQuery} from "react-query";
+import { MultipleCheckboxTableCell, NumberFieldTableCell, PriceFieldTableCell,  TextFieldTableCell } from "../../component/TableCells";
+import { QueryKeys } from "../../services/QueryKeys";
+import { OffersService } from "../../services/OffersService";
+import DateFnsUtils from '@date-io/date-fns';
+import { DatePicker, MuiPickersUtilsProvider } from "@material-ui/pickers";
+
+
+const offerService = new OffersService();
+const initialStartTime = new Date(); 
+const initialEndTime = new Date();
+export default function OffersView({}) {
+    const errorRef = useRef();
+
+    const columns = [
+        {
+          title: "Title",
+          field: "Title",
+          editComponent: (props) => TextFieldTableCell(props, errorRef),
+        },
+        {
+          title: "Description",
+          field: "Description",
+          editComponent: (props) => TextFieldTableCell(props, errorRef),
+        },
+        {
+            title: "Discount",
+            field: "Discount",
+            render: rowData => `${rowData.pricE}$`,
+            editComponent: (props) => PriceFieldTableCell(props, errorRef), 
+          },
+          {
+            title: 'Start Date Time',
+            type:"data",
+            field: 'StartDate',
+            initialEditValue: initialStartTime, editComponent: props => (
+              <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                      <DatePicker
+                          {...props}
+                          inputVariant="outlined"
+                          format="yyyy-MM-dd HH:mm:ss" 
+                          showTodayButton
+                          autoOk
+                          error={Boolean(props.helperText)}
+                      />
+                  </MuiPickersUtilsProvider>
+          )
+          },
+          {
+            title: 'End Date Time',
+            type:"date",
+            field: 'EndDate',
+            initialEditValue: initialEndTime,editComponent: props => (
+              <MuiPickersUtilsProvider utils={DateFnsUtils}>
+              <DatePicker
+                  {...props}
+                  inputVariant="outlined"
+                  format="yyyy-MM-dd HH:mm:ss" 
+                  showTodayButton
+                  autoOk
+                  error={Boolean(props.helperText)}
+              />
+          </MuiPickersUtilsProvider>
+          )
+          },
+      ];
+
+      return (
+        <CustomMaterialTable
+          title="Manage Offers"
+          columns={columns}
+          service={offerService}
+          queryKey={QueryKeys.OFFER}
+          errorRef={errorRef}
+        />
+      );
+  }
